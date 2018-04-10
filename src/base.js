1a93ddecd59bc767097474015967287a6eb678d9
Brenda Long <brenda@studio6thave.com>
2018-04-10 05:57:14 -0500

    added note in base

--- src/base.js
+++ src/base.js
@@ -3,12 +3,18 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 var app = firebase.initializeApp({
-    apiKey: "AIzaSyA60-RA1ZHZRXrmmAH-gsp1h7OpRFYWugY",
-    authDomain: "d18-demo.firebaseapp.com",
-    databaseURL: "https://d18-demo.firebaseio.com"
+    apiKey: "ReallyLongNumberGoesHere",
+    authDomain: "your-database.firebaseapp.com",
+    databaseURL: "https://your-database.firebaseio.com"
 });
 
 var db = firebase.database(app);
 var base = Rebase.createClass(db);
+
+/* for my demo. Be sure to put to include your own database
+apiKey: "AIzaSyA60-RA1ZHZRXrmmAH-gsp1h7OpRFYWugY",
+authDomain: "d18-demo.firebaseapp.com",
+databaseURL: "https://d18-demo.firebaseio.com"
+*/
 
 export default base;
